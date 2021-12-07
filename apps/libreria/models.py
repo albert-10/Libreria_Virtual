d427ade3fecd07ce0bc510ce9b76407c10d7cfe2
alberm <albertem101@gmail.com>
2021-12-07 15:01:37 -0500

    Se crearon todos los modelos en la BD

---
+++ apps/libreria/models.py
@@ -0,0 +1,55 @@
+import uuid
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.fields.related import ForeignKey
+
+User = get_user_model()
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=50)
+    autor = models.ForeignKey('Autor', on_delete=models.CASCADE)
+    nombre_editorial = models.CharField(max_length=50)
+    cantidad_paginas = models.IntegerField()
+    fecha_publicacion = models.DateField()
+    isbn = models.CharField(max_length=17)
+
+    def __str__(self):
+        return self.titulo
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=50)
+    nacionalidad = models.CharField(max_length=20)
+    fucha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
+    imagen = models.ImageField(upload_to='usuario_imagenes')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.get_username()
+
+    def get_username(self):
+        return self.user.get_username()
+
+class Review(models.Model):
+    CALIFICACION = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    )
+
+    usuario = ForeignKey(Usuario, related_name='reviews', on_delete=models.CASCADE)
+    fecha_creada = models.DateTimeField(auto_now_add=True)
+    opinion = models.TextField()
+    calificacion = models.IntegerField(choices=CALIFICACION)
+
+    def __str__(self):
+        return f"{self.usuario.get_username()} - {self.calificacion}"
+
+
