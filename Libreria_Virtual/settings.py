47c153e6f8dc7146f05c3a4583df96339eb30589
alberm <albertem101@gmail.com>
2021-12-08 07:15:37 -0500

    Se agrego base.html, navbar.html footer.html home.html y ya se listan los libros

--- Libreria_Virtual/settings.py
+++ Libreria_Virtual/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'Libreria_Virtual.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
--- apps/libreria/views.py
+++ apps/libreria/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Libro
 
-# Create your views here.
+""" class Home(generic.TemplateView):
+    template_name = 'libreria/home.html' """
+
+class HomeView(generic.ListView):
+    template_name='libreria/home.html'
+    queryset = Libro.objects.all()
